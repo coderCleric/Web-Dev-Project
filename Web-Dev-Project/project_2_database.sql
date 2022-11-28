c0bdc345b354cd7aae3f7f2daf63a9c58dc8ab69
o-oconnell <reamonoshea@gmail.com>
2022-11-27 19:12:54 -0700

    add sql file for current order table

--- Web-Dev-Project/project_2_database.sql
+++ Web-Dev-Project/project_2_database.sql
@@ -56,7 +56,7 @@ CREATE TABLE `cat_food` (
   `description` varchar(255) DEFAULT NULL,
   `number_stocked` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +65,7 @@ CREATE TABLE `cat_food` (
 
 LOCK TABLES `cat_food` WRITE;
 /*!40000 ALTER TABLE `cat_food` DISABLE KEYS */;
-INSERT INTO `cat_food` VALUES (1,'small cat','bladder support','$9000000','$3','Frisky Kidney Aid','Small cat food for cats with kidney troubles!\n',44),(2,'small cat','bladder support','$6000000','$3','Discount Frisky Kidney Aid','Small cat food for cats with kidney troubles!\n',55),(3,'large cat','n/a','$60','$4','Large Cat Standard','Normal cat food for large cats',12),(4,'small cat','n/a','$60','$4','Small Cat Standard','Normal cat food for small cats',12),(5,'medium cat','n/a','$60','$4','Medium Cat Standard','Normal cat food for medium cats',12);
+INSERT INTO `cat_food` VALUES (1,'small cat','bladder support','$9000000','$3','Frisky Kidney Aid','Small cat food for cats with kidney troubles!\n',44),(2,'small cat','bladder support','$6000000','$3','Discount Frisky Kidney Aid','Small cat food for cats with kidney troubles!\n',55),(3,'large cat','n/a','$60','$4','Large Cat Standard','Normal cat food for large cats',12),(4,'small cat','n/a','$60','$4','Small Cat Standard','Normal cat food for small cats',12),(5,'medium cat','n/a','$60','$4','Medium Cat Standard','Normal cat food for medium cats',12),(9,'tiger','raw','10','20','raw meat','carnivore food',123);
 /*!40000 ALTER TABLE `cat_food` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -146,6 +146,60 @@ LOCK TABLES `customer_shipping_address` WRITE;
 /*!40000 ALTER TABLE `customer_shipping_address` DISABLE KEYS */;
 INSERT INTO `customer_shipping_address` VALUES (1,'NM','Socorro','USA','801 Leroy',87801),(2,'NM','Socorro','USA','812 Leroy',87801),(3,'NM','Socorro','USA','899 Leroy',87801),(4,'NM','Socorro','USA','112 Leroy',87801),(5,'NM','Socorro','USA','111 California',87801);
 /*!40000 ALTER TABLE `customer_shipping_address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'joe','schmoe');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `debit_card_number` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `order_contents` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (3,'Joe Smith','joe@smith.com','12345','Delivered','111 leroy place','1:Frisky Kidney Aid 1:raw meat '),(4,'John Smith','joHn@smith.com','123455','Ordered','111 leroy','7:Frisky Kidney Aid ');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -233,4 +287,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-18 16:28:22
+-- Dump completed on 2022-11-27 19:11:39
